2a43b84656f7eafa026f01ff1783d0d7d070051b
Radoslav <rudix.lab@gmail.com>
2020-09-12 17:02:42 +0300

    c

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route
+            path="/:id"
+            exact
+            render={(props) => <About match={props.match} isIndex={false} />}
+          />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+const About = ({ match }) => {
+  return <div>{JSON.stringify(match.params.id)}</div>;
+};
